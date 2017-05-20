4a758166838985111323372f76c373ee673e486c
andremigueldasilvapinho <andremsp95@gmail.com>
2017-05-20 18:22:03 +0100

    --First version--

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,8 +1,14 @@
+import com.sun.xml.internal.org.jvnet.fastinfoset.FastInfosetException;
 import com.sun.xml.internal.ws.api.message.Message;
 
-import java.io.IOException;
+import java.io.*;
+import java.math.BigInteger;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 import java.util.Vector;
 
@@ -14,10 +20,14 @@ public class ServerThread extends Thread{
     private Socket socket = null;
     private int counter = 0;
     byte[] signature;
-
+    static String bitNumber;
     private final List<ServerThread> killList;
     private final List<ServerThread> serverActions;
+    public final static int SOCKET_PORT = 13267;
+    public final static String SERVER = "127.0.0.1";  
+    public final static String FILE_TO_RECEIVED = "~/Trasnferências/downloaded.txt";    // TODO: 20-05-2017  tem de se alterar isto para correr em windows também
 
+    public final static int FILE_SIZE = 6022386;    // TODO: 20-05-2017 verificar se este tamanho está correcto
     public UUID serverThreadID;
     private int maxLocalRetries;
 
@@ -60,7 +70,91 @@ public class ServerThread extends Thread{
     // TODO: 17-05-2017 login 
     // TODO: 17-05-2017 só sabe as suas transações 
 
-    public static void main(String[] args) throws IOException{
+    public static String bitRandom(){
+        Random rg = new Random();
+        int n = rg.nextInt(10);
+        return Integer.toBinaryString(n);
+    }
 
+    static String hextoBin(String s){
+        return new BigInteger(s,16).toString(2);
+    }
+
+
+    //return -1 caso não consiga vencer o challenge
+    //return 1 caso consiga vencer o challenge
+    // TODO: 20-05-2017 Substituir o return por uma nova coin dada ao user - Enviar mensagem para o utilizador especifico 
+    public int challenge() throws IOException {
+        int bytesRead;
+        int current = 0;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        Socket socket = null;
+        try{
+            socket = new Socket(SERVER,SOCKET_PORT);
+            System.out.println("Connecting...");
+            byte[] mybytearray = new byte[FILE_SIZE];
+            InputStream inputStream = socket.getInputStream();
+            fos = new FileOutputStream(FILE_TO_RECEIVED);
+            bos = new BufferedOutputStream(fos);
+            bytesRead = inputStream.read(mybytearray,0,mybytearray.length);
+            current = bytesRead;
+
+            do {
+                bytesRead = inputStream.read(mybytearray,current,(mybytearray.length-current));
+                if (bytesRead >= 0) current+=bytesRead;
+            }while (bytesRead>-1);
+
+            bos.write(mybytearray,0,current);
+            bos.flush();
+            System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + "bytes read)");
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (fos!=null)  fos.close();
+            if (bos!=null)  bos.close();
+            if (socket!=null)   socket.close();
+        }
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            FileInputStream fis = new FileInputStream(FILE_TO_RECEIVED);
+            byte[] dataBytes = new byte[FILE_SIZE];
+            int nread = 0;
+            while ((nread = fis.read(dataBytes))!=-1){
+                md.update(dataBytes,0,nread);
+            };
+            byte[] mdBytes = md.digest();
+            StringBuffer sb = new StringBuffer();
+            for (int i=0; i < mdBytes.length; i++){
+                sb.append(Integer.toString((mdBytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            String binary = hextoBin(sb.toString());
+            int flag = 0;
+            for (char x : binary.toCharArray()
+                 ) {
+                for (char y : bitNumber.toCharArray()
+                     ) {
+                    if (x == y) continue;
+                    else{
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag==1) {
+                    return -1;
+                }
+                else continue;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) throws IOException{
+        bitNumber = bitRandom();
     }
 }
