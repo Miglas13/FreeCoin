338421d0aa517bc732a766509cb9fbc971d7b926
Rui Santos <rui_17_santos@hotmail.com>
2017-05-27 00:50:50 +0100

    Transações

---
+++ src/Transaction.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Rui Santos on 27/05/2017.
+ */
+public class Transaction {
+
+    String destinatario = "";
+    int montante=0;
+    String PK, SK, PKD, SKD;
+
+
+
+    public static  void main (String argv[]){
+
+
+
+        System.out.println("");
+
+
+
+    }
+}
